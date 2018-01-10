e452cbb892e893cc0982c1f2d92334a50c9c9f00
Elizabeth Yeoh-Wang <eyw410@gmail.com>
2018-01-10 15:11:00 -0500

    fixed merge conflicts

--- controllers/users.js
+++ controllers/users.js
@@ -6,17 +6,17 @@ const Item = require('../models/schemas/item')
 */
 
 /*
-items: [{
+what gets passed into req.body:
+{ items: [{
         itemId: { type: Schema.ObjectId, ref: 'Item'},
-        quantity: Number,
-        price: Number
-      }]
+        quantity: Number
+      }] }
 */
 
 exports.checkout = (req, res, next) => {
   // one atomic step
+  // lock mutex (haha)
   var order = (req.body.items).slice()
-  console.log(order)
   var callbacks = 0
   // GIANT for loop; moves on to next for loop after callbacks = order.length
   for (thing in order) {
@@ -43,13 +43,12 @@ exports.checkout = (req, res, next) => {
             callbacks2++
             if (callbacks2 == order.length) {
               // add items array to this userId's orders array
-              User.findOneAndUpdate({ _id: req.params.userId }, {$push: {"orders": {items: order, purchasedDate: new Date(), isPaid: false, deliveredDate: null}}}, (err, item) => {
+              User.findOneAndUpdate({ _id: req.params.userId }, {$push: {"orders": {items: order, purchasedDate: new Date(), isPaid: false, deliveredDate: null}}}, (err, user) => {
                 if (err) return next(err)
-                if (!item) return res.status(404).send('Couldn\'t find item')
+                if (!user) return res.status(404).send('Couldn\'t find user')
                 console.log('added to user array')
+                return res.status(200).send("Done")
               })
-              
-              return res.status(200).send("Done")
             }
           })
           console.log("updated!")
@@ -75,68 +74,38 @@ exports.createUser = (req, res, next) => {
   if (!req.body.name) {
     return res.status(400).send('Must provide name')
   }
-<<<<<<< HEAD
   if (!req.body.address) {
     return res.status(400).send('Must provide address')
   }
   if (!req.body.classYear) {
     res.status(400).send('Must provide class year')
-=======
-  if (!req.body.phoneProvider) {
-   return res.status(400).send('Must provide phone provider') 
-  }
-  if (!req.body.phoneNumber) {
-   return res.status(400).send('Must provide phone number') 
-  }
-  if (!req.body.classYear) {
-   return res.status(400).send('Must provide class year') 
->>>>>>> 7df4e59f4aa2e75fa8e73a1223169ed91f35e9c9
   }
   const userData = {
     email: req.body.email,
     hash: req.body.password,
     name: req.body.name,
-<<<<<<< HEAD
     isAdmin: req.body.isAdmin,
     address: req.body.address,
     classYear: req.body.classYear,
     orders: [],
-
   }
   const newUser = new User(userData)
   newUser.save((err) => {
     if (err) return next(err) // res.status(500).send('Could not create')
-=======
-    phoneProvider: req.body.phoneProvider,
-    phoneNumber: req.body.phoneNumber,
-    classYear: req.body.classYear,
-  }
-  const newUser = new User(userData)
-  newUser.save((err) => {
-    if (err) return next(err)
->>>>>>> 7df4e59f4aa2e75fa8e73a1223169ed91f35e9c9
     return res.json(newUser)
   })
 }
 
 exports.getAllUsers = (req, res, next) => {
   User.find({}, (err, users) => {
-<<<<<<< HEAD
     if (err) return next(err) // res.status(500).send('Error: ' + err)
-=======
-    if (err) return next(err)
->>>>>>> 7df4e59f4aa2e75fa8e73a1223169ed91f35e9c9
     return res.json(users)
   })
 }
 
 exports.getUserById = (req, res, next) => {
   User.findById(req.params.userId, (err, user) => {
-<<<<<<< HEAD
     if (err) return next(err) // res.sendStatus(500)
-=======
-    if (err) return next(err)
->>>>>>> 7df4e59f4aa2e75fa8e73a1223169ed91f35e9c9
     if (!user) return res.status(404).send('No user with id: ' + req.params.userId)
     return res.json(user)    
   })
@@ -144,11 +113,7 @@ exports.getUserById = (req, res, next) => {
 
 exports.getUserByEmail = (req, res, next) => {
   User.findOne({ email: req.params.email }, (err, user) => {
-<<<<<<< HEAD
     if (err) return next(err) // res.sendStatus(500)
-=======
-    if (err) return next(err)
->>>>>>> 7df4e59f4aa2e75fa8e73a1223169ed91f35e9c9
     if (!user) return res.status(404).send('No user with email: ' + req.params.email)
     return res.json(user)    
   })
@@ -156,11 +121,7 @@ exports.getUserByEmail = (req, res, next) => {
 
 exports.updateUser = (req, res, next) => {
   User.findOneAndUpdate({ _id: req.params.userId }, req.body, {}, (err, user) => {
-<<<<<<< HEAD
     if (err) return next(err) // res.sendStatus(500)
-=======
-    if (err) return next(err)
->>>>>>> 7df4e59f4aa2e75fa8e73a1223169ed91f35e9c9
     if (!user) return res.status(404).send('Could not find user: ' + req.params.userId)
     return res.send('Updated user!')
   })
